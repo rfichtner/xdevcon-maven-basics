170c72a29e1316dd22ae106e15b502ab115215cf
Richard Fichtner <r.fichtner@xdev-software.de>
2019-07-16 17:17:38 +0200

    compile dependency

--- foodblog/src/main/java/com/xdevsoftware/Demo.java
+++ foodblog/src/main/java/com/xdevsoftware/Demo.java
@@ -1,12 +1,14 @@
 package com.xdevsoftware;
 
+import org.apache.commons.lang3.StringUtils;
+
+
 public class Demo
 {
 	
 	public static void main(final String[] args)
 	{
-		System.out.println("Hello world");
-		
+		System.out.println(StringUtils.abbreviate("Hello wolrd", 8));
 	}
 	
 }
